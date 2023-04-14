2825b1db8a13566122b4b389581c2119acf4cc87
moichubr <brunmoira@gmail.com>
2023-04-14 19:13:43 -0300

    validation

--- rick_and_morty/src/components/Form/validation.js
+++ rick_and_morty/src/components/Form/validation.js
@@ -1,29 +1,22 @@
 export default function validate (userData) {
     let errors = {};
         
-    if(userData.email === '') {
+    if(!userData.email) {
         errors.email= 'Ingresa tu email';
-    }
-    
-    if(userData.password === '') {
-        errors.password = 'Password vacía';
-    }
-
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/.test(userData.email)){
+    } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/.test(userData.email)){
         errors.email = 'Email inválido';
-    }
-
-    if(userData.email.length > 35) {
+    } else if (userData.email.length > 35) {
         errors.email = 'Email demasiado largo';
-    }
-
-    if(userData.password.length < 6 || userData.password.length > 10) {
+    } else { errors.email = '' };
+    
+    
+    if(!userData.password) {
+        errors.password = 'Password vacía';
+    } else if (userData.password.length < 6 || userData.password.length > 10) {
         errors.password = 'La contraseña debe tener entre 6 y 10 caracteres';
-    }
-
-    if(!/\d/.test(userData.password)) {
+    } else if (!/\d/.test(userData.password)) {
         errors.password = 'La contraseña debe tener al menos un número';
-    }
+    } else { errors.password= ''}
 
  return errors
 }
